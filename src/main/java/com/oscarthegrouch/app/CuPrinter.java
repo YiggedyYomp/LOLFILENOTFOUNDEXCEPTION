184f3409058e0ef2f29d2e428319b6de0ca1450d
Mitchel Wong <mitchelw@uvic.ca>
2017-04-03 13:28:56 -0700

    Now prints JoinColumn tokens that are found

--- src/main/java/com/oscarthegrouch/app/CuPrinter.java
+++ src/main/java/com/oscarthegrouch/app/CuPrinter.java
@@ -7,13 +7,17 @@ import com.github.javaparser.ast.CompilationUnit;
 public class CuPrinter {
 
     public static String cuPrint(){
+
 	String TableName = "";
 	String[] tableNameExtractor;//used to manipulate the string before storing it in TableName
+
         try{
 		FileInputStream in = new FileInputStream("/home/oscar14/test.java");
 		System.out.println("Shit worked yo");
 		CompilationUnit cu = JavaParser.parse(in);
 		String[] tokens = cu.toString().split("@");//split the string by @ symbols used to denote annotations
+
+		//for loop for extracting table name
 		for(int i=0;i<tokens.length;i++){
 			if(tokens[i].indexOf("Table")!=-1){
 				tableNameExtractor=tokens[i].split("\\(");
@@ -23,6 +27,15 @@ public class CuPrinter {
 				TableName=tableNameExtractor[0];
 			}
 		}
+
+		//for loop for extracting referenced tables
+		for(int i=0;i<tokens.length;i++){
+			if(tokens[i].indexOf("JoinColumn")!=-1){
+				System.out.println(tokens[i]);
+			}
+		}
+		System.out.println("END OF JoinColumns FOUND");
+		System.out.print("Table Name is: ");
      		return TableName;
 	}
 	catch(FileNotFoundException err){
